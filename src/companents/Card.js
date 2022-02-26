62b17fe6d0b3962b3db838c941b6f39710877a62
shakerdl <=>
2022-02-26 11:25:41 +0200

    fixed two API photos

--- src/companents/Card.js
+++ src/companents/Card.js
@@ -1,18 +1,17 @@
 import React from "react";
-import './Card.css'
+import "./Card.css";
 
-const Card = ({name,id,photo,children}) => {
+const Card = ({ name, id, pic, children }) => {
   return (
-    <div>
+    <div className="card">
       <h1>{name}</h1>
       <p>{id}</p>
-      <img src = {photo} />
-      <div className="ball-wrapper">
-          {children}
-      </div>
+      {pic.showing ?
+        <img className="pokemon-img" alt="pokemon-img" src={pic.url} />
+      : null}
+      <div className="ball-wrapper">{children}</div>
     </div>
   );
 };
 
 export default Card;
-{/* <p>{currPokemon.types[0].type.name}</p> */}
