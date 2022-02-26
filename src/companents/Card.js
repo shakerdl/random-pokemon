97a0d7ccc270fcf163c99bc87736a4264adb2001
shakerdl <=>
2022-02-26 22:05:32 +0200

    change css with flex

--- src/companents/Card.js
+++ src/companents/Card.js
@@ -7,7 +7,7 @@ const Card = ({ name, id, pic, children }) => {
       <h1>{name}</h1>
       <p>{id}</p>
       {pic.showing ?
-        <img className="pokemon-img" alt="pokemon-img" src={pic.url} />
+        <img  className="pokemon-img" alt="pokemon-img" src={pic.url} />
       : null}
       <div className="ball-wrapper">{children}</div>
     </div>
